22aae96bcf7a7fa866211d6241a8b812f0f0c102
vinaykangralkar <vinaykangralkar9@gmail.com>
2020-12-11 11:19:41 +1100

    Added styles

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({id, name, email}) => {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="image" src={`https://robohash.org/${id}?200x200`} />
+            <h1>{name}</h1>
+            <h3>{email}</h3>
         </div>
     );
 }
